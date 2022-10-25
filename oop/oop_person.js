c42b81f0a07713d9d18efbab98bb024480c17898
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-10-25 19:43:03 +0900

    JavaScript : OOP prototype method

--- oop/oop_person.js
+++ oop/oop_person.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // define class
 class Person {
   // initialize
@@ -19,8 +21,11 @@ class Person {
 
 // instance
 const tim = new Person('Tim', 29);
+
+// 프로토타입 메소드는 인스턴스로 호출.
 tim.sayHello(); // Hello, My name is Tim.
+
 console.log(tim.age); // 29
 
-// 정적메소드는 클래스로 호출. 인스턴스 생성하지 않아도 호출할 수 있음.
+// 정적 메소드는 클래스로 호출. 인스턴스 생성하지 않아도 호출할 수 있음.
 Person.sayHi(); // Hi!
