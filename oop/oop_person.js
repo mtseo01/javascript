625c64858b10a2f7140ee00818308c648d22e2e6
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-10-25 19:37:51 +0900

    JavaScript : OOP static method

--- oop/oop_person.js
+++ oop/oop_person.js
@@ -6,12 +6,21 @@ class Person {
     this.age = age;
   }
 
+  // prototype method
   sayHello() {
     console.log(`Hello, My name is ${this.name}.`);
+  }
+
+  // static method
+  static sayHi() {
+    console.log('Hi!');
   }
 }
 
 // instance
 const tim = new Person('Tim', 29);
-tim.sayHello();
-console.log(tim.age);
+tim.sayHello(); // Hello, My name is Tim.
+console.log(tim.age); // 29
+
+// 정적메소드는 클래스로 호출. 인스턴스 생성하지 않아도 호출할 수 있음.
+Person.sayHi(); // Hi!
