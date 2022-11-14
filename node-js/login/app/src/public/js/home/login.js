297e453a8bde360090f778760063ee5792127dbc
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-11-14 18:14:13 +0900

    save object of HTML using DOM

--- node-js/login/app/src/public/js/home/login.js
+++ node-js/login/app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 'use strict';
 
-console.log('Hello');
-console.log('Hi');
+const id = document.getElementById('id');
+const password = document.getElementById('password');
+const loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', login);
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+
+  console.log(req);
+}
